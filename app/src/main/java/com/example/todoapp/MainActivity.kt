fcbefad77ff545ef164c992f8454c13b2a87349d
unknown <hidahgakuru@gmail.com>
2022-06-06 08:34:32 -0700

    View model changes

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -6,45 +6,54 @@ import com.example.todoapp.R
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.todoapp.Users.User
 import com.example.todoapp.Users.UserDatabase
+import com.example.todoapp.Users.UserViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main. activity_main.*
 import kotlinx.android.synthetic.main.item_todo.*
 
 class MainActivity : AppCompatActivity() {
 
-    private  lateinit var  todoAdapter: TodoAdapter
+     lateinit var  todoAdapter: TodoAdapter
+     lateinit var viewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val db = Room.databaseBuilder(
-//            applicationContext,
-//            UserDatabase::class.java, "CheckList"
-//        ).build()
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        viewModel.readAllData.observe(this, Observer {
+            add.setOnClickListener {
+                val todoTitle = etTodoTitle.text.toString()
+                if (todoTitle.isNotEmpty()) {
+                    val todo = Todo(todoTitle)
+                    todoAdapter.add(todo)
+                    etTodoTitle.text.clear()
+                }}
 
+        })
 
         todoAdapter = TodoAdapter(mutableListOf())
 
         rvTOdoItem.adapter = todoAdapter
         rvTOdoItem.layoutManager = LinearLayoutManager(this)
 
-        add.setOnClickListener {
-            val todoTitle = etTodoTitle.text.toString()
-            if (todoTitle.isNotEmpty()) {
-                val todo = Todo(todoTitle)
-                todoAdapter.add(todo)
-                etTodoTitle.text.clear()
+//        add.setOnClickListener {
+//            val todoTitle = etTodoTitle.text.toString()
+//            if (todoTitle.isNotEmpty()) {
+//                val todo = Todo(todoTitle)
+//                todoAdapter.add(todo)
+//                etTodoTitle.text.clear()
+//            }}
 
 
-            }
-        }
-        delete.setOnClickListener{
+      delete.setOnClickListener{
             todoAdapter.delete()
     }
     }
