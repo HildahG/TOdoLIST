994849630065d6cc1e5b30a0bd1f9dee45796d0c
unknown <hidahgakuru@gmail.com>
2022-06-06 08:37:29 -0700

    changes

--- app/src/main/java/com/example/todoapp/MyDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.todoapp;
-
-
-import androidx.room.Database;
-
-//@Database(entities = [User::class], version = 1)
-//abstract class  MyDatabase : RoomDatabase() {
-//abstract fun userDao(): MyDao
-//        }
